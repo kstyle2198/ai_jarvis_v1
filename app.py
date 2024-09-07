ec3d42f19a44a479edf3d3c3ab2111fc2195f078
KimJongBae <jongbaekim0710@gmail.com>
2024-09-07 09:57:02 +0900

    test secrets

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import os 
 
+os.environ["GROQ_API_KEY"] == st.secrets["GROQ_API_KEY"]
 
 
 if __name__ == "__main__":
-    st.title("AI Jarvis")
+    st.title("AI Jarvis V2")
+
+    st.markdown(os.environ["GROQ_API_KEY"])
