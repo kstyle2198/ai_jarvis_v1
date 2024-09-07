81ff518883485590191b93e30b6d8bd52fbf51d2
KimJongBae <jongbaekim0710@gmail.com>
2024-09-07 10:56:45 +0900

    2

--- app.py
+++ app.py
@@ -4,12 +4,38 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
-my_groq = MyGroq()
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable import Runnable
+from langchain.schema.runnable.config import RunnableConfig
 
+
+GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
+
+class MyGroq:
+    def __init__(self):
+        pass
+
+    def chat_groq(self, query):
+        model = ChatGroq(temperature=0, model_name= "llama-3.1-8b-instant")   # "llama-3.1-8b-instant","llama-guard-3-8b"
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                (
+                    "system",
+                    "You're a very knowledgeable Machine Learning Engineer.",
+                ),
+                ("human", "{question}"),
+            ]
+        )
+        runnable = prompt | model | StrOutputParser()
+        res = runnable.invoke(query)
+        return res
+    
 
 if __name__ == "__main__":
     st.title("AI Jarvis V2")
+    my_groq = MyGroq()
 
     query = st.text_input("query")
     if st.button("Submit"):
